9262b307c8fdab6efa15441a453b79931eb1849f
root <terry.chad@gmail.com>
2019-09-11 11:16:05 -0700

    symlinks?

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,31 @@
-Dockerfile.dev
+ARG BUILD_FROM
+FROM $BUILD_FROM
+
+ENV LANG C.UTF-8
+
+# Install pre-requsites
+RUN apk update
+
+RUN apk add --no-cache openssh-server openssh-client python3 py3-pip
+RUN apk add --no-cache cifs-utils alsa-utils alsa-plugins alsaconf
+RUN apk add --no-cache mpd mpc ncmpc ympd espeak vim less bash egrep 
+RUN apk add --no-cache less strace 
+RUN apk add --no-cache --allow-untrusted apk/*apk
+
+RUN pip3 install paho-mqtt python-mpd2 pyaml youtube-dl
+
+#RUN apk add --no-cache pulseaudio pulseaudio-bash-completion
+#RUN apk add --no-cache pulseaudio-utils pulseaudio-alsa alsa-plugins-pulse
+
+# Copy data for add-on
+COPY run.sh /
+COPY etc/mpd.conf /etc
+COPY mqtt.py /bin
+COPY etc/server.yaml /etc
+COPY bin/play* /bin
+
+# Make executable
+RUN chmod a+x /run.sh /bin/mqtt.py /bin/play*
+
+# Let her rip...
+CMD [ "/run.sh" ]
--- run.sh
+++ run.sh
@@ -1,1 +1,49 @@
-run.sh.user
+#!/usr/bin/env bashio
+set -e
+
+bashio::log.info "MPD Server Starting..."
+
+# SSH
+#echo "Port 2222 >> /etc/ssh/sshd_config"
+
+#/bin/mkdir -p /root/.ssh
+#/bin/chmod -R 700 /root/.ssh
+
+passwd -u root
+
+#bashio::log.info "Copying SSH Configuration..."
+
+#cp etc/ssh* /etc
+#cp etc/moduli /etc
+#cp etc/.bash* /root
+#cp etc/.vimrc /root
+#cp etc/.inputrc /root
+
+#chmod 0600 /etc/ssh/ssh*
+
+#/usr/sbin/sshd -p 2222
+
+# MPD
+mkdir -p /data/mp3
+mkdir -p /data/video
+
+# why do i have to do this?
+chown root:audio /dev/snd/*
+
+#bashio::log.info "Mounting CIFS filesystems..."
+
+#mount //ftp/data/mp3   /data/mp3 -o username=data,password=pass
+#mount //ftp/data/video /data/video -o username=data,password=pass
+#mount //ftp//mpd /var/lib/mpd -o username=root,password=no
+
+# can also have this as jq, but i like... files
+
+cd /
+
+(sleep 30 && nohup ympd & ) &
+
+bashio::log.info "MPD Daemon starting .."
+
+python3 /bin/mqtt.py &
+
+mpd --no-daemon
